a3117284eb172f6c2fb6963780a3b8e116dc7343
D <jakubek.dominika@gmail.com>
2026-04-25 20:04:20 +0200

    Add optional TOTP secret to User class constructor

--- user_service/user.py
+++ user_service/user.py
@@ -2,9 +2,14 @@ import bcrypt
 import pyotp
 
 class User:
-    def __init__(self, username, password, role):
+    def __init__(self, username, password, role, totp_secret=None):
         self.username = username
         self.password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
         self.role = role
-        self.totp_secret = pyotp.random_base32()
+
+        if totp_secret:
+            self.totp_secret = totp_secret
+        else:
+            self.totp_secret = pyotp.random_base32()
+            
         

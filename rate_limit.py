5d2004d87a3b3c73504804ad94f8637f263b1901
D <jakubek.dominika@gmail.com>
2026-05-15 14:30:45 +0200

    Refactor rate limit function formatting

--- rate_limit.py
+++ rate_limit.py
@@ -8,7 +8,7 @@ def is_allowed(user):
     if user not in requests:
         requests[user] = []
 
-    # tylko ostatnie 10 sekund
+
     requests[user] = [t for t in requests[user] if now - t < 10]
 
     if len(requests[user]) > 5:
